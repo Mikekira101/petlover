78714c7a2717f779929a718b1d8475268cffbaef
Mikekira101 <oliverosmike13@gmail.com>
2025-05-13 21:44:19 -0500

    mongodb://localhost:27017

--- petlover-backend/config/db.js
+++ petlover-backend/config/db.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.set('strictQuery', true); // Soluciona la advertencia de Mongoose
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB conectado: ${conn.connection.host}`);
+        console.log('MongoDB conectado correctamente');
     } catch (error) {
-        console.error(`Error al conectar a MongoDB: ${error.message}`);
-        process.exit(1); // Detiene la aplicación si no se puede conectar
+        console.error('Error al conectar a MongoDB:', error);
+        process.exit(1); // Detiene la aplicación si no puede conectarse
     }
 };
 
