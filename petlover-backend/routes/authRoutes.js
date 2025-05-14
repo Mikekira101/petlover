52ba20f9841c7d7705124aec90ed80faa0004f5c
Mikekira101 <oliverosmike13@gmail.com>
2025-05-14 00:16:55 -0500

    user1

--- petlover-backend/routes/authRoutes.js
+++ petlover-backend/routes/authRoutes.js
@@ -12,12 +12,8 @@ router.post('/register', async (req, res) => {
         // Crear un nuevo usuario
         const newUser = new User({ name, email, password, phone });
         const savedUser = await newUser.save();
-
-        console.log('Usuario registrado exitosamente:', savedUser);
-
         res.status(201).json({ message: 'Usuario registrado exitosamente', user: savedUser });
     } catch (error) {
-        console.error('Error al registrar el usuario:', error);
         res.status(500).json({ message: 'Error al registrar el usuario', error: error.message });
     }
 });
